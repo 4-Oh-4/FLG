f4941c6151596136198a8057581ab18ba788a2a8
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-16 18:16:58 +0530

    Implementing PlayerControl

---
+++ FLG_GJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float movementSpeed = 1f;
+    Vector2 movementInput;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void movement(InputAction.CallbackContext callbackContext) {
+        //Debug.Log(callbackContext);
+        movementInput = callbackContext.ReadValue<Vector2>();
+
+    }
+    void FixedUpdate() {
+        // Apply movement
+        rb.linearVelocity = movementInput * movementSpeed;
+    }
+}
