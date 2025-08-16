ae1c8876304b0dd6781f717bf866f852a1cabf84
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-16 18:26:52 +0530

    Camera Script

---
+++ FLG_GJ/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform playerTransform;
+    //[SerializeField]float smoothSpeed = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate() {
+        Vector3 targetPos = new Vector3(playerTransform.position.x,transform.position.y,transform.position.z);
+        
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+        transform.position = targetPos;
+    }
+}
