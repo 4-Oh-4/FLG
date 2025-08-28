6ba293efffa79177733bb5580c318fe3cddcb86e
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-28 23:16:00 +0530

    Added the mini game

--- FLG_GJ/Assets/Scripts/AADARSH/LoadUnloadMiniGamesPlayerA.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/LoadUnloadMiniGamesPlayerA.cs
@@ -4,21 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class LoadUnloadMiniGamesPlayerA : MonoBehaviour {
     [SerializeField] Camera maincamera;
+    [SerializeField] GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void LoadMiniGame(string miniGameName) {
         Time.timeScale = 0f;
         maincamera.gameObject.SetActive(false);
-        GetComponent<PlayerInput>().enabled = false;
-        GetComponent<InteractWithDialogueA>().enabled = false;
+        player.SetActive(false);
         SceneManager.LoadScene(miniGameName,LoadSceneMode.Additive);
         Scene miniScene = SceneManager.GetSceneByName(miniGameName);
-        if (miniScene.IsValid())
-            SceneManager.SetActiveScene(miniScene);
+        
     }
     public void UnloadMiniGame() {
         Time.timeScale = 1f;
         maincamera.gameObject.SetActive(true);
-        GetComponent<PlayerInput>().enabled = true;
-        GetComponent<InteractWithDialogueA>().enabled = true;
+        player.SetActive(true);
     }
 }
--- FLG_GJ/Assets/Scripts/DIVI/DrugTheftMiniGame/GameManager_Drug.cs
+++ FLG_GJ/Assets/Scripts/DIVI/DrugTheftMiniGame/GameManager_Drug.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager_Drug : MonoBehaviour
 {
     public static GameManager_Drug Instance { get; private set; }
     public bool HasPackage { get; private set; }
+    public GameObject canvas;
 
     void Awake()
     {
@@ -11,6 +13,7 @@ public class GameManager_Drug : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
         Time.timeScale = 1f;
+        canvas.SetActive(false);
     }
 
     public void OnPackageCollected()
@@ -23,15 +26,27 @@ public class GameManager_Drug : MonoBehaviour
     {
         if (Time.timeScale == 0f) return;
         Debug.Log("Caught! Game Over.");
-        Time.timeScale = 0f; // quick pause; replace with UI later
-    }
+        StartCoroutine(ShowAndWaitRoutine());
 
+    }
+    private IEnumerator ShowAndWaitRoutine() {
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        Scene currentScene = gameObject.scene;
+
+        // Reload that scene (no unload needed)
+        SceneManager.UnloadSceneAsync("DrugTheftMiniGame");
+        SceneManager.LoadSceneAsync("DrugTheftMiniGame", LoadSceneMode.Additive);
+    }
     public void TryWin()
     {
         if (HasPackage)
         {
+            FindAnyObjectByType<LoadUnloadMiniGamesPlayerA>().UnloadMiniGame();
             Debug.Log("You escaped with the package! You win.");
-            Time.timeScale = 0f;
+            SceneManager.UnloadSceneAsync("DrugTheftMiniGame");
+            
+
         }
     }
 }
