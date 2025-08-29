0a49275afbb2d24050f2e193e63a95306fd80711
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-29 14:15:20 +0530

    Bug fiixes of black screen and Dialouge Box

--- FLG_GJ/Assets/Scripts/AADARSH/LoadUnloadMiniGamesPlayerA.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/LoadUnloadMiniGamesPlayerA.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class LoadUnloadMiniGamesPlayerA : MonoBehaviour {
     [SerializeField] Camera maincamera;
     [SerializeField] GameObject player;
+    [SerializeField] QuestUpdaterAct1A questupdater;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void LoadMiniGame(string miniGameName) {
         Time.timeScale = 0f;
@@ -12,10 +13,12 @@ public class LoadUnloadMiniGamesPlayerA : MonoBehaviour {
         player.SetActive(false);
         SceneManager.LoadScene(miniGameName,LoadSceneMode.Additive);
         Scene miniScene = SceneManager.GetSceneByName(miniGameName);
-        
+        questupdater.disablePointer();
     }
     public void UnloadMiniGame() {
         Time.timeScale = 1f;
+        StoryManagertAct1A.Instance.SetFlag("DrugTheftComplete", true);
+        questupdater.EnablePointer();
         maincamera.gameObject.SetActive(true);
         player.SetActive(true);
     }
