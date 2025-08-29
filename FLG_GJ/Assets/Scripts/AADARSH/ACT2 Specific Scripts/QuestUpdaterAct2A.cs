6b619f5b1758e83dc1f895f9f5a998e54e0223d9
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-29 23:33:55 +0530

    added the mini game loader in Act 2

--- FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/QuestUpdaterAct2A.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/QuestUpdaterAct2A.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class QuestUpdaterAct2A : MonoBehaviour
+public class QuestUpdaterAct2A : QuestUpdaterAct1A
 {
-    [SerializeField] Transform church;
-    [SerializeField] Transform Home;
+    [SerializeField] Transform churchb;
+    [SerializeField] Transform Homeb;
     [SerializeField] Transform Market;
     [SerializeField] Transform Disturbance;
     [SerializeField] Transform darius;
@@ -11,60 +11,60 @@ public class QuestUpdaterAct2A : MonoBehaviour
     [SerializeField] Transform tunnel;
     [SerializeField] Transform club;
 
-    [SerializeField] WindowQuestPointer_A questPointer_A;
+    [SerializeField] WindowQuestPointer_A questPointer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void disablePointer() {
-        questPointer_A.gameObject.SetActive(false);
+    public override void disablePointer() {
+        questPointer.gameObject.SetActive(false);
     }
-    public void EnablePointer() {
-        questPointer_A.gameObject.SetActive(true);
+    public override void EnablePointer() {
+        questPointer.gameObject.SetActive(true);
     }
-    public void Church() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+    public override void Church() {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = church;
+        questPointer.target = churchb;
     }
-    public void GoHome() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+    public override void GoHome() {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = Home;
+        questPointer.target = Homeb;
     }
     public void GoMarket() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = Market;
+        questPointer.target = Market;
     }
     public void CheckDisturbance() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = Disturbance;
+        questPointer.target = Disturbance;
     }
     public void MeetingArea() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = Meetingarea;
+        questPointer.target = Meetingarea;
     }
     public void DariusIntro() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = darius;
+        questPointer.target = darius;
     }
     public void Tunnel() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = tunnel;
+        questPointer.target = tunnel;
     }
     public void Club() {
-        if (questPointer_A.gameObject.activeInHierarchy == false) {
+        if (questPointer.gameObject.activeInHierarchy == false) {
             EnablePointer();
         }
-        questPointer_A.target = club;
+        questPointer.target = club;
     }
 }
