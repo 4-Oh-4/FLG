889cf1006dbd11dda954ec9cb90586631db907aa
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-29 20:00:20 +0530

    BlackScreenFix

--- FLG_GJ/Assets/Scripts/AADARSH/LoadMiniGameA.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/LoadMiniGameA.cs
@@ -5,13 +5,17 @@ public class LoadMiniGameA : MonoBehaviour
     [SerializeField] string MiniGameName;
     private int once = 0;
     [SerializeField] GameObject gameManager;
+    [SerializeField] ShowingStoryUpdates1 blackScreen;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")&&once==0) {
             once = 1;
+            blackScreen.ShowUpdate("sd");
             gameManager.GetComponent<LoadUnloadMiniGamesPlayerA>().LoadMiniGame(MiniGameName);
         }
         if (collision.CompareTag("Player") && once == 5) {
             once = 1;
+            blackScreen.ShowUpdate("sd");
+
             gameManager.GetComponent<LoadUnloadMiniGamesPlayerA>().LoadMiniGame(MiniGameName+"2");
         }
     }
