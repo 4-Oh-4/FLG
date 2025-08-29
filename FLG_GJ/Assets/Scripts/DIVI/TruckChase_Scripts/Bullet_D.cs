5048688eeb272b49964281d1906327c52b2c55e6
DIVI-Hectic4U <divs7835@gmail.com>
2025-08-29 20:51:59 +0530

    Bug Fix

--- FLG_GJ/Assets/Scripts/DIVI/TruckChase_Scripts/Bullet_D.cs
+++ FLG_GJ/Assets/Scripts/DIVI/TruckChase_Scripts/Bullet_D.cs
@@ -14,12 +14,12 @@ public class Bullet_D : MonoBehaviour
 
     void Update()
     {
-        transform.position += transform.up * speed * Time.deltaTime;
+        // Move along local X axis (red arrow)
+        transform.position += transform.right * speed * Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
         if (other.TryGetComponent<SedanAI_D>(out var sedan)) sedan.TakeDamage(damage);
         else if (other.TryGetComponent<JeepAI_D>(out var jeep)) jeep.TakeDamage(damage);
         else if (other.TryGetComponent<ArmoredVanAI_D>(out var van)) van.TakeDamage(damage);
