b843cc28da2d4085b9bbc652f3bbf39526679a04
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-31 00:33:44 +0530

    Jitna bhi hai jo bhi hai

--- FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/BedTimeSkipA.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/BedTimeSkipA.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class BedTimeSkipA : MonoBehaviour
 {
     [SerializeField] BlackScreenHouse blackscreen;
+    [SerializeField] string message;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            blackscreen.ShowUpdate("Few Weeks Later");
+            blackscreen.ShowUpdate(message);
         }
     }
 }
