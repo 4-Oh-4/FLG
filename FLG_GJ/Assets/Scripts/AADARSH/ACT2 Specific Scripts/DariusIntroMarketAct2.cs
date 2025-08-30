de371630c7704190460a9f06cc515b83feca66af
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-31 02:20:39 +0530

    Bug fixes

--- FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/DariusIntroMarketAct2.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/DariusIntroMarketAct2.cs
@@ -4,12 +4,13 @@ public class DariusIntroMarketAct2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private bool once = false;
+    [SerializeField] Collider2D colliderl;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             if (once) {
                 gameObject.GetComponent<NPC_A>().TriggerDialouge();
-                StoryManagertAct1A.Instance.SetFlag("TalkWithDarius", true); 
-            
+                StoryManagertAct1A.Instance.SetFlag("TalkWithDarius", true);
+                colliderl.enabled = false;
             }
         }
     }
