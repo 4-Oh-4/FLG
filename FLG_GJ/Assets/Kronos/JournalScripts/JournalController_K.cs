0398be784faa4e16b76ecdf002f098fec3544a87
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-09-01 04:04:49 +0530

    Added the journals

--- FLG_GJ/Assets/Kronos/JournalScripts/JournalController_K.cs
+++ FLG_GJ/Assets/Kronos/JournalScripts/JournalController_K.cs
@@ -7,7 +7,8 @@ public class JournalController : MonoBehaviour
     public CanvasGroup fadeGroup;   // assign your Black Image's CanvasGroup here
     public Button continueButton;   // assign your Continue Button here
     public float fadeDuration = 2f; // seconds to fade out
-
+    public GameObject journal;
+    public PlayerMovement_A playerMovement;
     void Start()
     {
         // Hook the button click to trigger FadeOut
@@ -28,7 +29,9 @@ public class JournalController : MonoBehaviour
             yield return null;
         }
         fadeGroup.alpha = 1f;
-
+        StoryManagertAct1A.Instance.SetFlag("JournalDone", true);
+        playerMovement.canMove = true;
+        journal.SetActive(false);
         // 👉 at this point screen is fully black
         // you could load the next scene here if needed
         // e.g.: SceneManager.LoadScene("NextScene");
