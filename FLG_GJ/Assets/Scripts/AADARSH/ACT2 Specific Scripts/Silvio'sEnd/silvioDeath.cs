abdfa38017da4b9197fa785020921016fb3a9a4e
DIVI-Hectic4U <divs7835@gmail.com>
2025-09-01 02:52:21 +0530

    Death of Silvio

--- FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/Silvio'sEnd/silvioDeath.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/Silvio'sEnd/silvioDeath.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class silvioDeath : MonoBehaviour {
+public class silvioDeath : MonoBehaviour
+{
     [Header("Detection Settings")]
     [Tooltip("The radius of the circle used to detect the player.")]
     [SerializeField] private float detectionRadius = 3f;
@@ -11,37 +12,53 @@ public class silvioDeath : MonoBehaviour {
     [Tooltip("The key the player must press to trigger the action.")]
     [SerializeField] private KeyCode interactionKey = KeyCode.E;
     [SerializeField] NPC_A cantalk;
+
+    // --- NEW ---
+    // Add a variable to hold the Animator component.
+    private Animator anim;
+
+    // --- NEW ---
+    // Awake is called when the script instance is being loaded.
+    private void Awake()
+    {
+        // Get the Animator component attached to this same GameObject.
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
-    private void Update() {
+    private void Update()
+    {
         // Cast a circle to see if the player is within range.
         Collider2D detectedCollider = Physics2D.OverlapCircle(transform.position, detectionRadius, playerLayer);
 
         // Check if we found a collider on the player layer.
-        if (detectedCollider != null) {
+        if (detectedCollider != null)
+        {
             // The player is in range. Now, check if they press the interaction key.
-            if (Input.GetKeyDown(interactionKey)) {
-                // --- CHANGED ACTION ---
-                // Instead of killing the player, this object will destroy itself.
+            if (Input.GetKeyDown(interactionKey))
+            {
+                Debug.Log("Player pressed E! " + gameObject.name + " is dying.");
                 cantalk.enabled = true;
-                Debug.Log("Player pressed E! " + gameObject.name + " is destroying itself.");
                 KillSilvio();
-                // 'gameObject' is a special variable that refers to the GameObject 
-                // this script component is attached to. Destroy() removes it from the scene.
-                //Destroy(gameObject);
             }
         }
     }
 
-    /// <summary>
-    
-    /// </summary>
-    private void OnDrawGizmosSelected() {
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
-    public void KillSilvio() {
-        //anim for death
-        
+
+    public void KillSilvio()
+    {
+        // --- MODIFIED ---
+        // Trigger the "Die" animation parameter we created.
+        if (anim != null)
+        {
+            anim.SetTrigger("Die");
+        }
+
         StoryManagertAct1A.Instance.SetFlag("DeathTalk", true);
         gameObject.GetComponent<NPC_A>().TriggerDialouge();
         gameObject.GetComponent<silvioDeath>().enabled = false;
