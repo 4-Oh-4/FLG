6962fca1edd56b3386a69caf6ff2d7ac8f02a3d9
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-09-01 03:07:53 +0530

    Bug fixes

--- FLG_GJ/Assets/Scripts/AADARSH/DJMiniGame/RoxanaTalkCollider.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/DJMiniGame/RoxanaTalkCollider.cs
@@ -4,11 +4,12 @@ public class RoxanaTalkCollider : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private int i = 1;
+    [SerializeField] Collider2D collider2;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player") && i == 0) {
             i++;
             gameObject.GetComponent<NPC_A>().TriggerDialouge();
-            
+            collider2.enabled = false;
         }
     }
     public void Iset() {
