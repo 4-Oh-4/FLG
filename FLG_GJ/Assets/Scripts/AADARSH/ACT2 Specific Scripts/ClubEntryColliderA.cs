260d16d4aa8f5fd9b0d785bfef75b6a49b9936bd
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-31 20:22:25 +0530

    Bug Fix

--- FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/ClubEntryColliderA.cs
+++ FLG_GJ/Assets/Scripts/AADARSH/ACT2 Specific Scripts/ClubEntryColliderA.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class ClubEntryColliderA : MonoBehaviour
 {
-    [SerializeField] private int i = 3;
+    [SerializeField] private int i = 20;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             if (i == 1) {
                 StoryManagertAct1A.Instance.SetFlag("ClubEntryAgain", true);
-                i++;
+                i=20;
             }
             if (i == 0){ StoryManagertAct1A.Instance.SetFlag("ClubEntry", true);
-            i++;
+            i=20;
             }
 
         }
     }
     public void setI() {
-        Debug.Log("ClubOpen");
-        i = 0;
+        if (!StoryManagertAct1A.Instance.GetFlag("ClubEntry"))
+            i = 0;
+        else i = 1;
     }
 }
